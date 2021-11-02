84ed540fb23cef588082e068b5298a503281bafb
xena <oksishukh@yahoo.com>
2021-11-02 16:58:08 +0200

    feat: add icons and style react audio player

--- src/containers/HomeContainer/index.js
+++ src/containers/HomeContainer/index.js
@@ -1,28 +1,50 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "@styles/components/HomeContainer.css";
 import ReactPlayer from "react-player";
-import upload from '@assets/icons/upload.svg'
+import upload from "@assets/icons/upload.svg";
+import arrowRight from "@assets/icons/arrow-right.svg";
+import speaker from "@assets/icons/audio.svg";
+import share from "@assets/icons/share.svg";
+import eye from "@assets/icons/eye.svg";
+import like from "@assets/icons/heart.svg";
 
 const HomeContainer = () => {
-
   return (
     <>
-      <div className='wrapper'>
-            <Container className="flex-row-center">
-                <div className='flex-column-align-start'>
-                <div className="info">
-                    <img src={upload} alt=""/>
-                    <h2 className='title'>Red Bull Racing</h2>
-                    <h6 className='hashtag'>#drift#redbull#rbwings#rbenergy</h6>
+      <div className="wrapper">
+        <Container className="flex-row-center">
+          <div className="flex-column-align-start">
+            <Row>
+              <Col
+                lg={6}
+                md={6}
+                sm={12}
+                xs={12}
+                className="flex-row-center-align info"
+              >
+                <img src={upload} alt="" className="me-3" />
+                <div className="me-5">
+                  <h2 className="title">Red Bull Racing</h2>
+                  <h6 className="hashtag">#drift#redbull#rbwings#rbenergy</h6>
                 </div>
-                <ReactPlayer
-                  
-                   url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
-                   </div>
-            </Container>
-            
-        </div>
+                <img className="icon-small" src={arrowRight} alt="" />
+              </Col>
+            </Row>
+            <ReactPlayer url="https://www.youtube.com/watch?v=ysz5S6PUM-U" />
+            <Row className="flex-row-center-align mt-2">
+              <Col lg={6} md={6} sm={6} xs={6}>
+                <img src={speaker} alt="" />
+              </Col>
+              <Col lg={6} md={6} sm={6} xs={6} className='flex-end'>
+                <img src={share} alt="" className="icon-small ms-4" />
+                <img src={eye} alt="" className="icon-small ms-4" />
+                <img src={like} alt="" className="icon-small ms-4" />
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
     </>
   );
 };
