56462a0b97ddffb47a208627191eb83f7959b4c6
xena <oksishukh@yahoo.com>
2021-11-02 17:46:59 +0200

    feat: add dropdown for user profile

--- src/containers/HeaderContainer/index.js
+++ src/containers/HeaderContainer/index.js
@@ -8,6 +8,13 @@ import history from "@assets/icons/history.svg";
 import category from "@assets/icons/buffer.svg";
 import play from "@assets/icons/play.svg";
 import add from "@assets/icons/group.svg";
+import wallet from "@assets/icons/wallet.svg";
+import coins from "@assets/icons/coins.svg";
+import settings from "@assets/icons/settings.svg";
+import web from "@assets/icons/web.svg";
+import headphones from "@assets/icons/headphones.svg";
+import info from "@assets/icons/info.svg";
+import doorOpen from "@assets/icons/door-open.svg";
 import "@styles/components/HeaderContainer.css";
 
 const HeaderContainer = () => {
@@ -21,11 +28,11 @@ const HeaderContainer = () => {
               INFOCOIN
             </h1>
           </Navbar.Brand>
-          <Nav className="ml-auto">
+          <Nav className="ml-auto flex-row-center-align">
             <Nav.Link>
               <img src={compass} className="icon-small" alt="" />
             </Nav.Link>
-            <Nav.Link className="me-4 ms-4">
+            <Nav.Link className="me-2 ms-4">
               <Dropdown>
                 <Dropdown.Toggle variant="success" id="dropdown-basic">
                   <img src={dropdown} className="icon-small" alt="" />
@@ -51,7 +58,52 @@ const HeaderContainer = () => {
               </Dropdown>
             </Nav.Link>
             <Nav.Link>
-              <img src={user} className="icon-small" alt="" />
+        
+              <Dropdown >
+                <Dropdown.Toggle variant="success" id="dropdown-basic" className='d-flex'>
+                <div className="icon-small circle-user" alt="">
+                  U
+              </div>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">
+                  <div className="icon-small circle-user" alt="">
+                  U
+                 </div>
+                   <span>@User122434</span>
+                   <span>Баланс IC <img src={coins} alt=""/>
+                     </span></Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                  <img src={wallet} className="me-3" alt=""/>
+                    Кошелек
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                  <img src={user} className="me-3" alt=""/>
+                    Аккаунт
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                  <img src={settings} className="me-3" alt=""/>
+                    Настройки
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                  <img src={web} className="me-3" alt=""/>
+                    Кеш и данные
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                  <img src={headphones} className="me-3" alt=""/>
+                    Поддержка
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                  <img src={info} className="me-3" alt=""/>
+                    Информация
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                  <img src={doorOpen} className="me-3" alt=""/>
+                    Выход
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
             </Nav.Link>
           </Nav>
         </Container>
