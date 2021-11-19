dec67da52f65036ac4784f9b74380d0f487be4d2
xena <oksishukh@yahoo.com>
2021-11-19 12:57:34 +0200

    feat: add captcha

--- src/containers/HomeContainer/index.js
+++ src/containers/HomeContainer/index.js
@@ -1,19 +1,28 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import "@styles/components/HomeContainer.css";
-import VideoSwiper from "@components/VideoSwiper";
-
-
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import '@styles/components/HomeContainer.css';
+import VideoSwiper from '@components/VideoSwiper/VideoSwiper';
+import ReCAPTCHA from "react-google-recaptcha";
 
 const HomeContainer = () => {
-
+  const recaptchaRef = React.createRef();
+  function onChange(value) {
+    console.log("Captcha value:", value);
+  }
   return (
+
     <>
-      <div className="wrapper">
-        <Container className="flex-row-center">
-             <VideoSwiper />
+      <Container className="wrapper">
+        <Container className="flex-row-column">
+          <VideoSwiper />
+          <ReCAPTCHA
+            ref={recaptchaRef}
+            // size="invisible"
+            sitekey="6LckU0MdAAAAADWY8V4yEJlDd-ibaCxEw9g7LbtI"
+            onChange={onChange}
+          />
         </Container>
-      </div>
+      </Container>
     </>
   );
 };
